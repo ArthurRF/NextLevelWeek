16d795474c5235dd09bad14299c196d8669ee81a
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-06-21 21:49:42 -0300

    Improves

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import 'reflect-metadata';
 import express, { response } from 'express';
+import { router } from './routes';
 import './database';
 
 const app = express();
 
-app.get("/test", (request, response) => {
-  return response.send("Olá NLW Get!");
-});
+app.use(express.json());
 
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW Post!");
-})
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Server is running!');
